3bfc790356fd02e21e695aa42d9f0e614ceb8a69
Александра <113774024+Alexandra-vic@users.noreply.github.com>
2023-03-19 21:32:16 +0600

    cleaning code

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,13 +1,34 @@
 from django.db import models
-from markets.models import Location, Product
+
 from django.urls import reverse, reverse_lazy
 
+from markets.models import Location, Product
+
+
 class Customer(models.Model):
-    first_name = models.CharField(max_length=100, blank=True, null=True, verbose_name='имя')
-    surname = models.CharField(max_length=100, blank=True, null=True, verbose_name='фамилия')
+    first_name = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True, 
+        verbose_name='имя',
+    )
+    surname = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True, 
+        verbose_name='фамилия',
+    )
     age = models.IntegerField()
-    gender = models.CharField(max_length=100, blank=True, null=True, verbose_name='пол')
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    gender = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True, 
+        verbose_name='пол',
+    )
+    location = models.ForeignKey(
+        Location, 
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return f'{self.first_name}  {self.surname}'
@@ -20,15 +41,30 @@ class Customer(models.Model):
         verbose_name_plural = 'Customers'
         ordering = ['-id']
 
-    
-  
-
 
 class Employee(models.Model):
-    first_name = models.CharField(max_length=100, blank=True, null=True, verbose_name='имя')
-    surname = models.CharField(max_length=100, blank=True, null=True, verbose_name='фамилия')
-    gender = models.CharField(max_length=100, blank=True, null=True, verbose_name='пол')
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    first_name = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True, 
+        verbose_name='имя',
+    )
+    surname = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True, 
+        verbose_name='фамилия',
+    )
+    gender = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True, 
+        verbose_name='пол',
+    )
+    location = models.ForeignKey(
+        Location, 
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.first_name
@@ -40,10 +76,29 @@ class Employee(models.Model):
 
 
 class Order(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product', verbose_name='продукт')    
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='employee', verbose_name='сотрудник')
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer', verbose_name='клиент')
-    date = models.DateField(auto_now_add=True, null=True, verbose_name='дата заказа')
+    product = models.ForeignKey(
+        Product, 
+        on_delete=models.CASCADE, 
+        related_name='product', 
+        verbose_name='продукт',
+    )    
+    employee = models.ForeignKey(
+        Employee, 
+        on_delete=models.CASCADE, 
+        related_name='employee', 
+        verbose_name='сотрудник',
+    )
+    customer = models.ForeignKey(
+        Customer, 
+        on_delete=models.CASCADE, 
+        related_name='customer', 
+        verbose_name='клиент',
+    )
+    date = models.DateField(
+        auto_now_add=True, 
+        null=True, 
+        verbose_name='дата заказа',
+    )
 
     def __str__(self):
         return self.product.name
